6d0a8ece17170687004ab0af88b8e96ee6b2606b
Manas gupta <iammanas95@gmail.com>
2018-11-10 13:55:40 +0530

    new users and alert

--- server.js
+++ server.js
@@ -17,14 +17,33 @@ app.use(express.static(publicpath));
 io.on('connection',(socket)=>{       
 console.log('new user connected');
 
+socket.emit('newMessage',{
+	from:'Admin',
+	text:'Welcome to chat app',
+	createdAt:new Date().getTime()
+});
+
+socket.broadcast.emit('newMessage',{
+	from:'Admin',
+	text:'New User joined',
+	createdAt:new Date().getTime()
+});
+
+
 socket.on('createMessage',(message)=>{
 	console.log('Message created',message);
-	io.emit('newMessage',{
+	
+io.emit('newMessage',{
     from:message.from,
     text:message.text,
     createdAt:new Date().getTime()
 	});
-})
+// socket.broadcast.emit('newMessage',{
+//    	    from:message.from,
+//    		text:message.text,
+//    	 	createdAt:new Date().getTime()
+//    	});
+});
  
 socket.on('disconnect',()=>{
        console.log('User was Disconnected');
